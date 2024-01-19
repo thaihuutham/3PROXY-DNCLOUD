99c15c2db96d0a4d67c5779049c26793ce8381ff
thaihuutham <40970590+thaihuutham@users.noreply.github.com>
2024-01-19 10:35:14 +0700

    Update 3proxy-dncloud.sh

--- 3proxy-dncloud.sh
+++ 3proxy-dncloud.sh
@@ -93,7 +93,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=40000
-LAST_PORT=40400
+LAST_PORT=41600
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
